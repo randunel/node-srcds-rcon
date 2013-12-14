3f4e63784fc199d7ded5154015c8ac0c617fdf06
Mihai Ene <dkrandu@yahoo.com>
2013-12-14 12:10:07 +0000

    Fixed commands saved on addition

--- dev/auth.js
+++ dev/auth.js
@@ -4,8 +4,9 @@ var rcon = new Rcon('127.0.1.1:27015', 'test');
 
 rcon.connect(function() {
     //console.log(rcon);
-    rcon.xbox_throttlespoof(function(err, res) {
-        console.log('res', res);
+    console.log('sv_airaccelerate object after initialization', rcon.sv_airaccelerate());
+    rcon.sv_airaccelerate(10, function(err, res) {
+        console.log('sv_airaccelerate server response', res);
         rcon.changelevel('de_dust2', function(err, res) {
             !err && console.log('Changed map to de_dust2');
         });
