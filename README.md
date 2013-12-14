d2559a00168184724d21f5f0c36fdc480a55b7cb
Mihai Ene <dkrandu@yahoo.com>
2013-12-14 11:28:26 +0000

    Added native RCON commands to Rcon instance

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ node-srcds-rcon
 ===============
 
 Node.JS high-level wrapper for SRCDS's remote console (RCON) https://developer.valvesoftware.com/wiki/RCON
+
+
--- dev/auth.js
+++ dev/auth.js
@@ -2,8 +2,13 @@ var Rcon = require('../lib/Rcon.js');
 
 var rcon = new Rcon('127.0.1.1:27015', 'test');
 
-rcon.connect();
-
-rcon.exec('status', function(res) {
-    console.log('Got res', res);
+rcon.connect(function() {
+    //console.log(rcon);
+    rcon.xbox_throttlespoof(function(err, res) {
+        console.log('res', res);
+        rcon.changelevel('de_dust2', function(err, res) {
+            !err && console.log('Changed map to de_dust2');
+        });
+    });
 });
+
