067ab1de3cb789576eef4c9381cd5931abf21b55
Mihai Ene <dkrandu@yahoo.com>
2014-06-18 21:55:19 +0100

    Added config object to initialization. Cvar list can be disabled by config

--- dev/auth.js
+++ dev/auth.js
@@ -1,13 +1,18 @@
 var Rcon = require('../lib/Rcon.js');
 
-var rcon = new Rcon('192.168.0.2:27015', 'test');
+//var rcon = new Rcon('192.168.0.2:27015', 'test');
+var rcon = new Rcon({
+    address: '192.168.0.2:27015',
+    password: 'test',
+    initCvars: false
+});
 
 rcon.connect(function() {
-    //console.log(rcon);
-    console.log('sv_airaccelerate object after initialization', rcon.sv_airaccelerate());
-    rcon.sv_airaccelerate(10, function(err, res) {
+    //rcon.sv_airaccelerate(10, function(err, res) {
+    rcon.runCommand('sv_airaccelerate 10', function(err, res) {
         console.log('sv_airaccelerate server response', res);
-        rcon.changelevel('de_dust2', function(err, res) {
+        //rcon.changelevel('de_dust2', function(err, res) {
+        rcon.runCommand('changelevel de_dust2', function(err, res) {
             !err && console.log('Changed map to de_dust2');
         });
     });
